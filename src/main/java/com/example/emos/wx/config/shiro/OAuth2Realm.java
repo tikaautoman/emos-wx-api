d046e0669002e1b4688cef68dd2947813f4a439d
yyyyasuo <1165599099@qq.com>
2023-04-10 16:50:04 +0800

    实现Shiro授权功能

--- src/main/java/com/example/emos/wx/config/shiro/OAuth2Realm.java
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Realm.java
@@ -10,6 +10,8 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class OAuth2Realm extends AuthorizingRealm {
     @Autowired
@@ -25,11 +27,14 @@ public class OAuth2Realm extends AuthorizingRealm {
      * 授权（验证权限时调用）
      */
     @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals){
-        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection collection){
+        TbUser user = (TbUser) collection.getPrimaryPrincipal();
+        int userId = user.getId();
         //查询用户的权限列表
+        Set<String> permsSet = userService.searchUserPermissions(userId);
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
         //把权限列表添加到info对象中
-
+        info.setStringPermissions(permsSet);
         return info;
     }
     /**
